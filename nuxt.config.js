89621a202ef19b032de10ac432ed88363494b45c
SphericalKat <amolele@gmail.com>
2021-04-02 20:25:40 +0530

    refactor(meta): update name and description
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sphericalk.at',
+    title: 'SphericalKat',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: "SphericalKat's musings" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
