ad59c492daf76194f75756c13995733d4d0bcb70
SphericalKat <amolele@gmail.com>
2021-04-29 18:53:08 +0530

    chore(build): set up tailwind with postcss
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: false, // we have manually configured purgecss
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
