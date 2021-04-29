cfd6eb1b9e3ac29e75fe1d6ad635e0e898e4ea76
SphericalKat <amolele@gmail.com>
2021-04-29 18:13:27 +0530

    chore: set up askama templating engine
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@
 #[macro_use]
 extern crate rocket;
 
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    name: String
+}
+
+#[get("/")]
+fn index() -> IndexTemplate {
+    IndexTemplate { name: "Amogh".to_owned() }
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes!());
+    rocket::ignite().mount("/", routes!(index)).launch();
 }
