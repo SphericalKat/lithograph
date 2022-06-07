5b440a02c7da40e6e93fe450bb6e471363d31a22
Amogh Lele <amolele@gmail.com>
2022-06-08 03:56:43 +0530

    refactor(treewide): long overdue styles overhaul
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,3 @@
-// Configure purgecss plugin
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./templates/**/*.html",
-    // './src/**/*.jsx',
-    // './src/**/*.js'
-  ],
-
-  // This is the function used to extract class names from your templates
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-    return broadMatches.concat(innerMatches);
-  },
-});
-
 // Export all plugins our postcss should use
 module.exports = {
   plugins: [
@@ -27,7 +6,6 @@ module.exports = {
 
     ...(process.env.NODE_ENV === "production"
       ? [
-          purgecss,
           require("cssnano")({
             preset: "default",
           }),
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 module.exports = {
   purge: false, // we have manually configured purgecss
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    './templates/**/*.html'
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        'sans': ['JetBrains Mono'],
-        'mono': ['JetBrains Mono'],
-        'display': ['JetBrains Mono'],
-        'body': ['JetBrains Mono'],
-      },
-
-      colors: {
-        'selected-grey': '#8f93a2',
-        'amber': '#ffcb6b',
-      }
+      // fontFamily: {
+      //   // 'sans': ['JetBrains Mono'],
+      //   'mono': ['JetBrains Mono'],
+      //   // 'display': ['JetBrains Mono'],
+      //   // 'body': ['JetBrains Mono'],
+      // },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
